1fbb1a06c29968959a2c344b051203ddbb8b6acd
Anton Miroshkin <anton.miroshkin@gmail.com>
2019-10-28 13:38:09 +0300

    Added specific exception handling

--- BestPracticesHandlingErrors/BestPracticesHandlingErrors/Program.cs
+++ BestPracticesHandlingErrors/BestPracticesHandlingErrors/Program.cs
@@ -7,6 +7,11 @@ namespace BestPracticesHandlingErrors
 {
     class Program
     {
+
+        //Source 
+        //https://blog.elmah.io/csharp-exception-handling-best-practices/?utm_source=csharpdigest&utm_medium=email&utm_campaign=featured
+
+
         static void Main(string[] args)
         {
 
@@ -26,6 +31,31 @@ namespace BestPracticesHandlingErrors
                 {
                     Console.WriteLine($"{o.Key} : {o.Value}");
                 } 
+            }
+
+
+            int numberOfHands = 9;
+            //Catch specific exceptions
+            try
+            {
+                if (numberOfHands%2 != 0)
+                {
+                    throw new ArgumentException("");
+
+                }
+            }
+            catch (ArgumentException e)
+            {
+                e.Data.Add("numberOfHands", numberOfHands);
+                Console.WriteLine($"Incorrect numberOfHands value - {e.Data["numberOfHands"]}");
+            }
+            catch (NullReferenceException e)
+            {
+
+            }
+            catch (Exception e)
+            {
+
             }
 
         }
